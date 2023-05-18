f9d9b46290e478eb81fdbf5b96b277e31280da90
pankaj443 <pankajofficial443@gmail.com>
2023-05-18 14:32:58 +0530

    example project for espresso

---
+++ app/src/androidTest/java/com/sample/browserstack/samplecalculator/EnsureInputTests.java
@@ -0,0 +1,50 @@
+package com.sample.browserstack.samplecalculator;
+
+import androidx.test.filters.SmallTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * Espresso tests to ensure that editText box is updated appropriately
+ * whenever buttons are clicked
+ */
+
+@SmallTest
+@RunWith(AndroidJUnit4.class)
+public class EnsureInputTests {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule =
+            new ActivityTestRule<MainActivity>(MainActivity.class);
+
+    private MainActivity mainActivity;
+
+    @Before
+    public void setUp() {
+        mainActivity = activityRule.getActivity();
+    }
+
+    @Test
+    public void ensureSingleInputIsHandled() {
+        onView(withId(R.id.buttonOne)).perform(click());
+        onView(withId(R.id.editText)).check(matches(withText("1")));
+    }
+
+    @Test
+    public void ensureMultipleInputIsHandled() {
+        onView(withId(R.id.buttonOne)).perform(click());
+        onView(withId(R.id.buttonTwo)).perform(click());
+        onView(withId(R.id.editText)).check(matches(withText("12")));
+    }
+}
